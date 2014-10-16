1c3f682f80501f2e743f0bf1d0fb8578459dc941
Joseph Sabato <Reticulin@Josephs-MacBook-Air.local>
2014-10-16 15:46:19 -0700

    fixed password on migration

--- db/migrate/20141016213254_create_users.rb
+++ db/migrate/20141016213254_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :last_name
       t.string :email
       t.string :username
-      t.password :password
+      t.string :password
       t.timestamps
     end
   end
