f59935c34f7ad8456e982d3974e55bd3b87037a6
sarahlshowers <sarahlshowers@gmail.com>
2014-10-17 11:10:01 -0700

    adding geolocation funtionality with a view to layering in places aspects to work for our app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,7 @@
 //= require turbolinks
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
+// $(function(){ $(document).foundation(); });
 
 // google maps - no longer needed with places!
 // function initialize() {
@@ -31,40 +31,91 @@ $(function(){ $(document).foundation(); });
 //   google.maps.event.addDomListener(window, 'load', initialize);
 
 // google places - radar search request
+// var map;
+// var service;
+
+// function initialize() {
+//   map = new google.maps.Map(document.getElementById('map-canvas'), {
+//     center: new google.maps.LatLng(37.7833, -122.4167),
+//     zoom: 15
+//   });
+
+//   infoWindow = new google.maps.InfoWindow();
+//   service = new google.maps.places.PlacesService(map);
+
+//   google.maps.event.addListenerOnce(map, 'bounds_changed', performSearch);
+// }
+
+// function performSearch() {
+//   var request = {
+//     bounds: map.getBounds(),
+//     keyword: 'dev bootcamp'
+//   };
+//   service.radarSearch(request, callback);
+// }
+
+// function callback(results, status) {
+//   if (status != google.maps.places.PlacesServiceStatus.OK) {
+//     alert(status);
+//     return;
+//   }
+//   for (var i = 0, result; result = results[i]; i++) {
+//     var marker = new google.maps.Marker({
+//       map: map,
+//       position: result.geometry.location
+//     });
+//   }
+// }
+
+// google.maps.event.addDomListener(window, 'load', initialize);
+
+//geolocation
 var map;
-var service;
 
 function initialize() {
-  map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: new google.maps.LatLng(37.7833, -122.4167),
-    zoom: 15
-  });
-
-  infoWindow = new google.maps.InfoWindow();
-  service = new google.maps.places.PlacesService(map);
-
-  google.maps.event.addListenerOnce(map, 'bounds_changed', performSearch);
-}
-
-function performSearch() {
-  var request = {
-    bounds: map.getBounds(),
-    keyword: 'dev bootcamp'
+  var mapOptions = {
+    zoom: 18
   };
-  service.radarSearch(request, callback);
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+
+  // Try HTML5 geolocation
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
+
+      var infowindow = new google.maps.InfoWindow({
+        map: map,
+        position: pos,
+        content: 'Found You!',
+      });
+
+      map.setCenter(pos);
+    }, function() {
+      handleNoGeolocation(true);
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleNoGeolocation(false);
+  }
 }
 
-function callback(results, status) {
-  if (status != google.maps.places.PlacesServiceStatus.OK) {
-    alert(status);
-    return;
+function handleNoGeolocation(errorFlag) {
+  if (errorFlag) {
+    var content = 'Error: The Geolocation service failed.';
+  } else {
+    var content = 'Error: Your browser doesn\'t support geolocation.';
   }
-  for (var i = 0, result; result = results[i]; i++) {
-    var marker = new google.maps.Marker({
-      map: map,
-      position: result.geometry.location
-    });
-  }
+
+  var options = {
+    map: map,
+    position: new google.maps.LatLng(60, 105),
+    content: content
+  };
+
+  var infowindow = new google.maps.InfoWindow(options);
+  map.setCenter(options.position);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
