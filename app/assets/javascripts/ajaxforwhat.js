5d6cd32015fb61d65ba06c25bf95d36e0f40d6ce
Joseph Sabato <Reticulin@Josephs-MacBook-Air.local>
2014-10-17 12:08:21 -0700

    Added ajax call for sign in and sign up form

--- app/assets/javascripts/ajaxforwhat.js
+++ app/assets/javascripts/ajaxforwhat.js
@@ -1,5 +1,5 @@
 $(function() {
-
+// -----------Sign In-------------------
   $("#signIn").on("click", function(e){
     e.preventDefault();
     $.ajax({
@@ -12,6 +12,18 @@ $(function() {
     })
   });
 
+  $("$signinBtn").on("click", function(e){
+    e.preventDefault();
+    $.ajax({
+      url: sessions_path,
+      type: 'post'
+    }).done(function(){
+      console.log("created new session for user")
+    }).fail(function(){
+      console.log("did not create new session for user")
+    })
+  });
+// -----------Sign Up-------------------
   $("#signUp").on("click", function(e){
     e.preventDefault();
     $.ajax({
@@ -24,6 +36,18 @@ $(function() {
     })
   });
 
+  $("#signupBtn").on("click", function(e){
+    e.preventDefault();
+    $.ajax({
+      url: users_path,
+      type: 'post'
+    }).done(function(){
+      console.log("signed up")
+    }).fail(function(){
+      console.log("didn't sign up")
+    })
+  });
+// -----------Sign Out-------------------
   $("#signOut").on("click", function(e){
     e.preventDefault();
     $.ajax({
