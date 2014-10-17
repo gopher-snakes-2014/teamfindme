b380a1563214dd71157aac849cea9f97b71e333c
Joseph Sabato <Reticulin@Josephs-MacBook-Air.local>
2014-10-16 17:16:41 -0700

    added association for note and set up digest for user password

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,13 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :username, presence: true, uniqueness: true
   has_secure_password
+
+  if Rails.env.production?
+
+    validates_presence_of :password, :on => :create
+    validates_confirmation_of :password
+    validates_presence_of :password_digest
+
+  end
+
 end
