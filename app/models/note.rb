794b0835e4da12705438d92a858fe868770b8eed
Apprentice <bob.maestri@gmail.com>
2014-10-18 14:23:13 -0700

    FIXED BUGSGSGSG

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,6 @@
 class Note < ActiveRecord::Base
   belongs_to :user
-  validates :comment, presence: true
+  # validates :comment, presence: true
   validates :longitude, presence: true
   validates :latitude, presence: true
 end
