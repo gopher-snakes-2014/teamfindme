166eb51554194a22c53048f898b7a6b92d6f9881
Apprentice <bob.maestri@gmail.com>
2014-10-20 11:06:55 -0700

    schema setup for heroku

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,14 @@
 class Note < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :image
+  has_attached_file :image,
+                    :storage => :s3,
+                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates :comment, presence: true
+
+  def s3_credentials
+    {:bucket => "findmeimages", :access_key_id => "AKIAJ6OO5KTQ2CYI4HYA", :secret_access_key => "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"}
+  end
+
 end
