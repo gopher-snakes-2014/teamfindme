9699e99a2bd6c504da52023ab1e3faf1b88d5d76
sarahlshowers <sarahlshowers@gmail.com>
2014-10-20 19:22:41 -0700

    aligning after pull to prevent conflict

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,7 @@ module ApplicationHelper
   def signed_in?
       session[:current_user_id]
     end
-
-    def current_user
-      @user ||= session[:current_user_id] && User.find(session[:current_user_id])
-    end
-
-    # def avatar_url(user)
-    #   gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    #   "http://gravatar.com/avatar/#{gravatar_id}.png}"
-    # end
+  def current_user
+    @user ||= session[:current_user_id] && User.find(session[:current_user_id])
+  end
 end
