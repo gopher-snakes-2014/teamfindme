0ffa1f5a39c32b47e442932f42d22e27165ece30
sarahlshowers <sarahlshowers@gmail.com>
2014-10-21 15:02:25 -0700

    styling carousel to sit on smaller screen image size in carousel set to a "default" size

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,7 +1,7 @@
 class Note < ActiveRecord::Base
   belongs_to :user
   has_attached_file :image,
-            :default_url => "http://png-3.findicons.com/files/icons/329/simple/128/search.png",
+            :default_url => "http://findicons.com/files/icons/575/pleasant/128/search.png",
             :s3_protocol => "https",
             :s3_host_name => "s3-us-west-2.amazonaws.com",
             :style => { :medium => "300x300>", :thumb => "100x100>" },
