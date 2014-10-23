6c1594b103f7a54511613f2a410975a831d4cfc1
Joseph Sabato <Reticulin@Josephs-MacBook-Air.local>
2014-10-22 18:19:42 -0700

    fixed voting

--- app/assets/javascripts/NoteWidget/notewidget.js
+++ app/assets/javascripts/NoteWidget/notewidget.js
@@ -10,7 +10,6 @@ Note.prototype = {
   },
 
   appendCarouselNote: function(notes, url, username, userId) {
-
     var voters = notes.voters;
     if (userId === undefined || null) {
       $(this.carousel).append(
@@ -28,13 +27,13 @@ Note.prototype = {
   },
 
   makeNoteTemplate: function(note, url, username, userId) {
-    return "<div><div class=note_styling><form class=like><input type=hidden value="+note.id+"><input type=hidden value="+userId+"><a href=#><img class=likesrc src=/assets/like.png style=float:right></form><h4>"+ username +"</h4><img src="+ url +"><p>"+ note.comment +"</p></div></div>";
+    return "<div class='note_styling'><div><form class='like'><input type='hidden' value="+note.id+"><input type='hidden' value="+userId+"><a href='#'><img class='likesrc' src='/assets/like.png' style='float:right'></form><h4 id='anne'>"+ username +"</h4><img src="+ url +"><p>"+ note.comment +"</p></div></div>";
   },
   makeNoteTemplateVoted: function(note, url, username, userId){
-    return "<div><div class=note_styling><form class=liked><input type=hidden value="+note.id+"><input type=hidden value="+userId+"><a href=#><img class=likedsrc src=/assets/liked.png style=float:right></form><h4>"+ username +"</h4><img src="+ url +"><p>"+ note.comment +"</p></div></div>";
+    return "<div class=note_styling><div><form class=liked><input type=hidden value="+note.id+"><input type=hidden value="+userId+"><a href=#><img class=likedsrc src=/assets/liked.png style=float:right></form><h4>"+ username +"</h4><img src="+ url +"><p>"+ note.comment +"</p></div></div>";
   },
   makeNoteTemplateNoLog: function(note, url, username){
-    return "<div><div class=note_styling><h4>"+ username +"</h4><img src="+ url +"><p>"+ note.comment +"</p></div></div>";
+    return "<div class=note_styling><h4>"+ username +"</h4><img src="+ url +"><p>"+ note.comment +"</p></div></div>";
   }
 
 
