8a6d555447b12b8be334de58801e45331c46bd86
Herine Chin <herine.chin@gmail.com>
2014-10-22 17:47:20 -0700

    merged

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,22 +2,24 @@ $( document ).ready(function() {
   console.log( "ready!" );
 
   if(/iP(hone|ad)/.test(window.navigator.userAgent)) {
-      var elements = document.querySelectorAll('button');
-      var emptyFunction = function() {};
-      for(var i = 0; i < elements.length; i++) {
-        elements[i].addEventListener('touchstart', emptyFunction, false);
-      }
+    var elements = document.querySelectorAll('button');
+    var emptyFunction = function() {};
+    for(var i = 0; i < elements.length; i++) {
+      elements[i].addEventListener('touchstart', emptyFunction, false);
     }
+  }
 
   $(document).foundation();
 
   var noteWidget = new Note();
 
   google.maps.event.addDomListener(window, 'load', initialize(noteWidget));
-
+  $('#seeAllNotes').click(function(){
+    $('.center').fadeIn('slow')
+    setSlickCarousel('.center');
+  })
   // google.maps.event.addDomListener(window, 'load', initialize());
 
-  setSlickCarousel('.center');
 
   $('body').on('click', 'form.like', function(e){
     item = $(this).children()[2]
